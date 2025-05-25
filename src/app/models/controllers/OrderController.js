68a51e7491082e0e353710558272c4496bdb2f60
Domingos244 <domingosrodrigo333@gmail.com>
2025-05-25 20:23:24 -0300

    pedidos

---
+++ src/app/models/controllers/OrderController.js
@@ -0,0 +1,72 @@
+import * as Yup from 'yup';
+import Order from '../schemas/Order';
+import Product from '../../models/Product';
+import Category from '../../models/Category';
+
+class OrderController {
+    async store(request, response) {
+        const schema = Yup.object({
+            product: Yup.array().required().of(
+                Yup.object({
+                    id: Yup.number().required(),
+                    quantify: Yup.number().required(),
+                }),
+            ),
+        });
+
+        try {
+            schema.validateSync(request.body, { abortEarly: false });
+        } catch (err) {
+            return response.status(400).json({ error: err.errors });
+        }
+
+        const { products } = request.body;
+
+        const productsIds = products.map((product) => product.id);
+
+        const findProducts = await Product.findAll({
+            where: {
+                id: productsIds,
+            },
+            include: [
+                {
+                    model: Category,
+                    as: 'category',
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const formattedProducts = findProducts.map((product) => {
+            const productIndex = products.findIndex(item => item.id == product.id);
+
+
+            const newProduct = {
+                id: product.id,
+                name: product.name,
+                category: product.category.name,
+                price: product.price,
+                url: product.url,
+                quantify: products[productIndex].quantify,
+            };
+
+            return newProduct;
+        });
+
+
+        const order = {
+            user: {
+                id: request.userId,
+                name: request.userName,
+            },
+            product: formattedProducts,
+            status: 'Pedido realizado',
+        };
+
+        const createdOrder = await Order.create(order);
+
+        return response.status(201).json(createdOrder);
+    }
+}
+
+export default new OrderController();
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
-import authMiddleware from './middlewares/auth';
+import authMiddleware from './app/middlewares/auth';
 
 
 import UserController from './app/models/controllers/UserControllers';
 import SessionController from './app/models/controllers/SessionController';
 import ProductController from './app/models/controllers/ProductController';
 import CategoryController from './app/models/controllers/CategoryController copy';
+import OrderController from './app/models/controllers/OrderController';
 
 const routes = new Router();
 
@@ -22,6 +23,9 @@ routes.get('/products', ProductController.index);
 
 routes.post('/categories', CategoryController.store);
 routes.get('/categories', CategoryController.index);
+
+routes.post('/orders', OrderController.store);
+
 
 
 export default routes;
